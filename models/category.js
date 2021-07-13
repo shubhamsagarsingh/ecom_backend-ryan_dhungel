375ce18a0f541540c3f615fb6bcd228ecc9604d0
shubhamsagarsingh <shubham.sagar.singh@gmail.com>
2021-07-13 21:13:38 +0530

    Complete BACKEND from Ryan Dhungel ecommerce project

---
+++ models/category.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      require: true,
+      maxlength: 32
+    },
+  }, 
+  
+  {
+    timestamps: true,
+  }
+)
+
+
+
+module.exports = mongoose.model('Category', categorySchema)
